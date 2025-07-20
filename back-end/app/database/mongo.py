1de768ca00f136f5e68bcf4b5a5c4ea2d963592d
Hans Chris <hanschris@Hanss-Mac-mini.local>
2025-07-20 21:38:45 +0100

    Stop tracking mongo.py

--- back-end/app/database/mongo.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://hanschris96:Hcts.8000@clusterhans.w9d0bwn.mongodb.net/?retryWrites=true&w=majority&appName=ClusterHans")
-client = AsyncIOMotorClient(MONGO_URI)
-
-db = client["book_barker"]  # use your actual database name
-bookings_collection = db["bookings"]  # use your desired collection name
